52050f670a006abbe4ebdfc4ef8ce19c75b2c49e
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-10-21 18:04:19 +0300

    init

---
+++ ChicknRabbitFarm/App/ChicknRabbitFarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChicknRabbitFarmApp.swift
+//  ChicknRabbitFarm
+//
+//  Created by Serhii Babchuk on 21.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChicknRabbitFarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CRLaunchView()
+        }
+    }
+}
---
+++ Pods/Target Support Files/Pods-ChicknRabbitFarm/Pods-ChicknRabbitFarm-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChicknRabbitFarm : NSObject
+@end
+@implementation PodsDummy_Pods_ChicknRabbitFarm
+@end
